bdac6ce69eb0d6655dc275de3e053b4d68370063
ukhan64 <92057998+ukhan64@users.noreply.github.com>
2021-10-23 21:28:13 -0400

    old laptop commit

--- src/testingprojectforgit/Class1.java
+++ src/testingprojectforgit/Class1.java
@@ -4,5 +4,6 @@ public class Class1 {
 public static void main(String[] args) {
 	System.out.println("UK is king");
 	System.out.println("life is beautiful");
+	System.out.println("old laptop");
 }
 }
